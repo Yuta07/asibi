672d7eead8bb433eaf812457e2e88a520485cdc3
yutaka.miyazaki <mono.12dev@gmail.com>
2020-09-22 01:52:47 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    sw: 'service-worker.js',
-    runtimeCaching: true,
+    // disable: process.env.NODE_ENV === 'development',
+    // register: true,
+    // sw: 'service-worker.js',
+    // runtimeCaching: true,
   },
 });
