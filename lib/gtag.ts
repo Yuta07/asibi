46ad318ad2e3fbc579c9bc43e459f5d74b45f0c3
yutaka.miyazaki <mono.12dev@gmail.com>
2022-01-12 18:10:13 +0900

    fix ID

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,7 +1,8 @@
 export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
+export const UNIVERSAL_ID = process.env.NEXT_PUBLIC_GOOGLE_UNIVERSAL_ID || ''
 
 export const pageview = (path: string) => {
-	window.gtag('config', GA_ID, {
+	window.gtag('config', UNIVERSAL_ID, {
 		page_path: path,
 	})
 }
