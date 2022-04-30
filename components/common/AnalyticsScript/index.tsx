b34b1ec15c4e7aa7d1b45afebb8da83efacf28c4
yutaka.miyazaki <mono.12dev@gmail.com>
2022-04-30 23:53:04 +0900

    build: upgrade-interactive

--- components/common/AnalyticsScript/index.tsx
+++ components/common/AnalyticsScript/index.tsx
@@ -1,18 +1,16 @@
 import Script from 'next/script'
 
-import { GA_TRACKING_ID, UNIVERSAL_ID } from '@/lib/gtag'
+import { GA_TRACKING_ID } from '@/lib/gtag'
 
 export const AnalyticsScript = () => {
 	return (
 		<>
-			<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${UNIVERSAL_ID}`} />
 			<Script id="gtag-init" strategy="afterInteractive">
 				{`
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
 
-					gtag('config', '${UNIVERSAL_ID}');
 					gtag('config', '${GA_TRACKING_ID}');
 			`}
 			</Script>
