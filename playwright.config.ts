30b17d646da8b31fae0aab4aea870e9c182be29e
yutaka.miyazaki <mono.12dev@gmail.com>
2022-06-29 18:52:51 +0900

    ci: fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,26 +1,22 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 
-let baseURL = 'http://localhost:3000'
-{
-	const { ROOT_TEST_URL } = process.env
-	if (ROOT_TEST_URL) {
-		if (ROOT_TEST_URL.at(-1) === '/') baseURL = ROOT_TEST_URL.slice(0, -1)
-		else baseURL = ROOT_TEST_URL
-	}
-}
-
 const config: PlaywrightTestConfig = {
 	testDir: 'tests/e2e',
 	timeout: 10000,
 	forbidOnly: !!process.env.CI,
 	use: {
 		actionTimeout: 5000,
-		baseURL,
+		baseURL: 'http://localhost:3000',
 		browserName: 'chromium',
-		headless: !!process.env.CI,
+		headless: false,
 		viewport: { width: 1280, height: 960 },
 		ignoreHTTPSErrors: true,
 		screenshot: 'only-on-failure',
+	},
+	webServer: {
+		command: 'yarn dev',
+		port: 3000,
+		reuseExistingServer: !process.env.CI,
 	},
 	projects: [
 		{
