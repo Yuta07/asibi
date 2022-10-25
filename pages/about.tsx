af80f05e873df12efef034c39ba6515f126ba5b2
yutaka.miyazaki <mono.12dev@gmail.com>
2022-10-25 14:51:17 +0900

    feat: update about screen

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,8 @@
+import { Accounts } from '@/components/common/Accounts'
 import { Layout } from '@/components/common/Layout'
+import { PageTitle } from '@/components/common/PageTitle'
 import { SEO } from '@/components/common/SEO'
+import styles from '@/styles/About.module.scss'
 
 import type { ReactElement } from 'react'
 
@@ -7,6 +10,27 @@ export default function About() {
 	return (
 		<>
 			<SEO title="About" description="About Me" openGraph={{ url: '/profile', type: 'article' }} />
+			<div className={styles.container}>
+				<PageTitle title="About" />
+				<div>
+					<p>
+						Hi, I’m a web frontend engineer living in Kanagawa from Aichi, Japan.
+						<br />I like Udon and walking. I love football.
+					</p>
+					<p className={styles.paragraph}>
+						After a year of SaaS customization and feature development experience, I became responsible for development
+						in the frontend area.
+						<br />
+						Currently focusing mainly on React / Next.js.
+					</p>
+				</div>
+				<div>
+					<h2 className={styles.subHeading}>Accounts</h2>
+					<div className={styles.accoutns}>
+						<Accounts />
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
