fb5e087097087f6bb72fde46eecc40dfa43a7537
yutaka.miyazaki <mono.12dev@gmail.com>
2023-06-13 23:13:55 +0900

    feat: improve menu animation when open

--- src/components/ui/NavigationProgress/index.tsx
+++ src/components/ui/NavigationProgress/index.tsx
@@ -1,34 +1,11 @@
 'use client'
 
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect, useMemo, useState } from 'react'
+import { useRouteChange } from '@/hooks/useRouteChange'
 
 import s from './styles.module.css'
 
-let prevPath: string | null = null
-
 export const NavigationProgress = () => {
-	const [isRouteChangeComp, setIsRouteChanged] = useState<boolean | null>(false)
-
-	const pathname = usePathname()
-	const searchParams = useSearchParams()
-
-	useEffect(() => {
-		prevPath = pathname
-		setIsRouteChanged(true)
-
-		const timer = setTimeout(() => {
-			setIsRouteChanged(null)
-		}, 500)
-
-		return () => {
-			clearTimeout(timer)
-		}
-	}, [pathname, searchParams])
-
-	const isReStaredRouteChange = useMemo(() => {
-		return prevPath !== pathname && isRouteChangeComp === null
-	}, [pathname, isRouteChangeComp])
+	const { isReStaredRouteChange, isRouteChangeComp } = useRouteChange()
 
 	const progressClassName = isRouteChangeComp === null ? s.initProgress : isRouteChangeComp ? s.progressFin : s.progress
 
