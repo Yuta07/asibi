aebca32c1f11abe7f9ca032ea905610d6c8c528b
yutaka.miyazaki <mono.12dev@gmail.com>
2023-10-30 00:30:27 +0900

    style: fix root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					media="all"
 					rel="stylesheet"
 				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							const storageTheme = window.localStorage.getItem('theme')
+							const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+							const root = window.document.documentElement
+
+							console.log('storageTheme', storageTheme)
+							console.log('isDark', isDark)
+
+							root.setAttribute('data-theme', storageTheme === 'system' ? (isDark ? 'dark' : 'light') : storageTheme || 'dark')
+						`,
+					}}
+				/>
 				<GoogleAnalyticsScript />
 			</head>
 			<body>
