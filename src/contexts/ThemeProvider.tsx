12dab845210495ad211edb79af097b0388a86ec4
yutaka.miyazaki <mono.12dev@gmail.com>
2023-11-04 01:59:26 +0900

    fix

--- src/contexts/ThemeProvider.tsx
+++ src/contexts/ThemeProvider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Script from 'next/script'
 import { createContext, memo, ReactNode, useContext } from 'react'
 
 import { useTheme } from '../hooks/useTheme'
@@ -14,14 +15,13 @@ export const ThemeDispatchContext = createContext<DispatchType>({ handleChangeTh
 // テーマのちらつき防止
 const _ThemeScript = () => {
 	return (
-		<script
+		<Script
+			strategy="beforeInteractive"
 			dangerouslySetInnerHTML={{
 				__html: `
 						const storageTheme = window.localStorage.getItem('theme')
             const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
             const root = window.document.documentElement
-
-						console.log('storageTheme', storageTheme)
 
             root.setAttribute('data-theme', storageTheme === 'system' ? (isDark ? 'dark' : 'light') : storageTheme || 'dark')
 			`,
