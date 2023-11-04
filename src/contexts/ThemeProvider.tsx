07378007106ab41fa6622366315643d7e697e07e
asibi3Q <mono.12dev@gmail.com>
2023-11-04 14:39:19 +0900

    Develop (#439)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * feat: add suspense for ga
    
    * fix
    
    * style: delete root prefers-color-theme
    
    * fix

--- src/contexts/ThemeProvider.tsx
+++ src/contexts/ThemeProvider.tsx
@@ -15,18 +15,16 @@ export const ThemeDispatchContext = createContext<DispatchType>({ handleChangeTh
 const _ThemeScript = () => {
 	return (
 		<script
-			type="text/javascript"
 			dangerouslySetInnerHTML={{
 				__html: `
 					const storageTheme = window.localStorage.getItem('theme')
 					const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 					const root = window.document.documentElement
 
-					console.log('DOMContentLoaded context')
-
 					root.setAttribute('data-theme', storageTheme === 'system' ? (isDark ? 'dark' : 'light') : storageTheme || 'dark')
 				`,
 			}}
+			type="text/javascript"
 		/>
 	)
 }
